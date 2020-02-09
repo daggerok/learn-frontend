24b1cb7d05aa6a20d112905455c2e2a1ebc70d3f
Deployment Bot (from Travis CI) <deploy@travis-ci.org>
2020-02-09 01:43:42 +0000

    Deploy daggerok/learn-frontend to github.com/daggerok/learn-frontend.git:gh-pages

---
+++ precache-manifest.93f870c878cea5e65ab1ab2d2d0b1c94.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "fdc65412d95a4db1ad47324627f4fe59",
+    "url": "/learn-frontend/index.html"
+  },
+  {
+    "revision": "819ead0d626f0c7debc6",
+    "url": "/learn-frontend/static/css/main.d1b05096.chunk.css"
+  },
+  {
+    "revision": "944b9cf7615e1069e314",
+    "url": "/learn-frontend/static/js/2.53a8c91c.chunk.js"
+  },
+  {
+    "revision": "99bd0487192ec9e7d9ee8fbbd91ee444",
+    "url": "/learn-frontend/static/js/2.53a8c91c.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "819ead0d626f0c7debc6",
+    "url": "/learn-frontend/static/js/main.ae4ef1e5.chunk.js"
+  },
+  {
+    "revision": "261ac478ac0717fdd9e5",
+    "url": "/learn-frontend/static/js/runtime-main.5850175b.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/learn-frontend/static/media/logo.5d5d9eef.svg"
+  }
+]);
